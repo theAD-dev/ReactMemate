d5b028d45916bdaf8359e86f82c39183bc0fb901
raj Verma <dev@thead.com.au>
2024-04-10 19:09:16 +0530

    new-update-build67

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import ForgotPassword from './components/layout/Login/ForgotPassword';
 import CheckMail from './components/layout/Login/CheckMail';
 import PasswordReset from './components/layout/Login/PasswordReset';
 import OnboardingCreate from './components/layout/onboarding/OnboardingCreate';
-import Requestdemo from './components/layout/requestdemo/Requestdemo';
 import SignUp from './components/layout/requestdemo/SignUp';
 import SelectCountry from './components/layout/requestdemo/SelectCountry';
 import CompanyName from './components/layout/requestdemo/CompanyName';
 import SelectDate from './components/layout/requestdemo/SelectDate';
 import AllSet from './components/layout/requestdemo/AllSet';
+import Create from './components/layout/onboarding/Create';
+import Verifymail from './components/layout/onboarding/Verifymail';
+import Companyname from './components/layout/onboarding/Companyname';
+import Regionalsettings from './components/layout/onboarding/Regionalsettings';
+import Discovermemate from './components/layout/onboarding/Discovermemate';
 
 
 function App() {
@@ -23,12 +27,16 @@ function App() {
           <Route path="/check-mail" element={<CheckMail />} />
           <Route path="/password-reset" element={<PasswordReset />} />
           <Route path="/onboarding" element={<OnboardingCreate />} />
-          <Route path="/requestdemo" element={<Requestdemo />} />
-          <Route path="/signup" element={<SignUp />} />
+          <Route path="/requestdemo" element={<SignUp />} />
           <Route path="/selectcountry" element={<SelectCountry />} />
           <Route path="/companyname" element={<CompanyName  />} />
           <Route path="/selectdate" element={<SelectDate />} />
           <Route path="/allset" element={<AllSet />} />
+          <Route path="/onboarding-create" element={<Create />} />
+          <Route path="/verify-mail" element={<Verifymail />} />
+          <Route path="/company-name" element={<Companyname />} />
+          <Route path="/regional-settings" element={<Regionalsettings />} />
+          <Route path="/discover-memate" element={<Discovermemate />} />
         </Routes>
     </AuthProvider>  
   );
