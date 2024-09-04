35a6574aca6bc5f3256326b7a3b701b3b0ce21a7
Sourav Santra <55660986+Sourav59580@users.noreply.github.com>
2024-09-04 13:05:51 +0000

    feature: update create index file

--- src/components/Business/features/sales-features/merges-calculation/create-merge-calculation/index.js
+++ src/components/Business/features/sales-features/merges-calculation/create-merge-calculation/index.js
@@ -1,7 +1,25 @@
-import clsx from 'clsx'
-import React from 'react'
-import { Button } from 'react-bootstrap'
+import clsx from 'clsx';
+import React, { useEffect, useState } from 'react';
+import { Button, InputGroup, ListGroup, Modal } from 'react-bootstrap';
+import { QuestionCircle } from 'react-bootstrap-icons';
+import Form from 'react-bootstrap/Form';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { useParams } from 'react-router-dom';
+import { toast } from 'sonner';
+import { useMutation } from '@tanstack/react-query';
+
 import style from './create-merge-calculation.module.scss';
+import mergeItemsImg from "../../../../../../assets/images/img/merge-items.svg";
+
+// Validation schema
+const schema = yup
+  .object({
+    title: yup.string().required("Title is required"),
+    description: yup.string().required("Description is required"),
+  })
+  .required();
 
 const CreateMergeCalculation = ({ unique_id, selectItem, setMerges }) => {
   const handleOpen = () => {
