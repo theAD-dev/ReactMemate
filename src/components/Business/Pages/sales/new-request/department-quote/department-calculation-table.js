a4e23b3aee4170bac3e25114061d0cd690c98996
soura1121 <sourav.1121@zenmonk.tech>
2024-09-16 19:59:01 +0530

    removed unused

--- src/components/Business/Pages/sales/new-request/department-quote/department-calculation-table.js
+++ src/components/Business/Pages/sales/new-request/department-quote/department-calculation-table.js
@@ -3,7 +3,7 @@ import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
 import { useQuery } from '@tanstack/react-query';
 import { Check, ChevronDown, GripVertical, Trash } from 'react-bootstrap-icons';
 
-import { deleteMergeQuote, getCalculationByReferenceId, getDepartments, getMergeItemsByUniqueId, getQuoteByUniqueId } from '../../../../../../APIs/CalApi';
+import { getCalculationByReferenceId, getDepartments } from '../../../../../../APIs/CalApi';
 import SelectComponent from './select-component';
 import { DepartmentQuoteTableRowLoading } from './department-quote-table-row-loading';
 import './select-component.css';
@@ -514,10 +514,9 @@ const DepartmentCalculationTable = ({ setTotals, setPayload, defaultDiscount, xe
     useEffect(() => {
         if (departments?.length) {
             const keyIndexMap = {};
-            const reformattedMerges = merges.map((merge, index) => {
+            merges?.forEach((merge, index) => {
                 const alias = romanize(index + 1);
-
-                merge?.calculators?.map(cal => {
+                merge?.calculators?.forEach(cal => {
                     keyIndexMap[cal.calculator] = alias;
                 });
             });
--- src/components/Business/Pages/sales/new-request/department-quote/index.js
+++ src/components/Business/Pages/sales/new-request/department-quote/index.js
@@ -26,7 +26,6 @@ const CustomOption = (props) => {
 };
 
 const DepartmentQuote = React.memo(({ payload, setPayload, setTotals, refetch, preExistCalculation, preExistMerges, setMergeDeletedItems }) => {
-  const [isDiscountDisplayed, setIsDiscountDisplayed] = useState(true);
   const [paymentCollection, setPaymentCollection] = useState('');
   const [selectedManagers, setSelectedManagers] = useState([]);
   const [editedReference, setEditedReference] = useState('');
