9338abcfa15fb0773ac1cb189559e862f2254bd1
soura1121 <sourav.1121@zenmonk.tech>
2024-10-05 19:50:17 +0530

    client-history-info

--- src/components/Business/Pages/clients/client-order-history/client-order-history-table.js
+++ src/components/Business/Pages/clients/client-order-history/client-order-history-table.js
@@ -1,18 +1,21 @@
 import clsx from 'clsx';
-import React, { forwardRef, useState } from 'react';
+import React, { forwardRef, useRef, useState } from 'react';
 import { Tag } from 'primereact/tag';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { ControlledMenu, useClick } from '@szhsin/react-menu';
+import '@szhsin/react-menu/dist/index.css';
 
 import style from './client-order-history.module.scss';
 import NoDataFoundTemplate from '../../../../../ui/no-data-template/no-data-found-template';
-import { ArrowLeftCircle, FilePdf, Files, FileText, InfoCircle, Link45deg, PlusSlashMinus } from 'react-bootstrap-icons';
+import { ArrowLeftCircle, FilePdf, Files, FileText, InfoCircle, Link45deg, ListUl, PlusSlashMinus } from 'react-bootstrap-icons';
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'sonner';
 import { fetchduplicateData } from '../../../../../APIs/SalesApi';
 import { ProgressSpinner } from 'primereact/progressspinner';
 import { useMutation } from '@tanstack/react-query';
 import { projectsToSalesUpdate } from '../../../../../APIs/management-api';
+import { CloseButton } from 'react-bootstrap';
 
 const ClientOrderHistoryTable = forwardRef(({ selected, setSelected, clientOrders, isPending }, ref) => {
   const navigate = useNavigate();
@@ -99,6 +102,41 @@ const ClientOrderHistoryTable = forwardRef(({ selected, setSelected, clientOrder
     }
   }
 
+  const InfoBodyTemplate = (rowData) => {
+    const ref = useRef(null);
+    const [isOpen, setOpen] = useState(false);
+    const anchorProps = useClick(isOpen, setOpen);
+    return <React.Fragment>
+      <InfoCircle color='#667085' size={16} className='cursor-pointer' ref={ref} {...anchorProps} />
+      <div style={{ position: 'fixed', top: '40%', left: '40%' }}>
+        <ControlledMenu
+          state={isOpen ? 'open' : 'closed'}
+          anchorRef={ref}
+          onClose={() => setOpen(false)}
+          menuStyle={{ padding: '24px 24px 20px 24px', width: '365px' }}
+        >
+          <div className='d-flex justify-content-between mb-4'>
+            <div className='BoxNo'>
+              <div>
+                <ListUl color='#FFFFFF' size={24} />
+              </div>
+            </div>
+            <CloseButton onClick={() => setOpen(false)} />
+          </div>
+
+          {
+            rowData?.indexes?.map(index => (
+              <div className={clsx('mb-3 text-start')}>
+                <h1 className={style.department}>{index[1]}</h1>
+                <h5 className={style.subdepartment}>{index[2]}</h5>
+              </div>
+            ))
+          }
+        </ControlledMenu>
+      </div>
+    </React.Fragment>
+  }
+
   const duplicateBodyTemplate = (rowData) => {
     return <>
       {rowData?.unique_id === isDuplicating
@@ -130,7 +168,7 @@ const ClientOrderHistoryTable = forwardRef(({ selected, setSelected, clientOrder
       <Column header="Invoice" body={InvoiceBodyTemplate} style={{ minWidth: '114px' }}></Column>
       <Column header="Quote" body={quoteBodyTemplate} style={{ minWidth: '160px' }}></Column>
       <Column header="History" body={<FileText color='#667085' size={16} />} bodyClassName={"text-center"} style={{ minWidth: '70px' }}></Column>
-      <Column header="Info" body={<InfoCircle color='#667085' size={16} />} bodyClassName={"text-center"} style={{ minWidth: '68px' }}></Column>
+      <Column header="Info" body={InfoBodyTemplate} bodyClassName={"text-center"} style={{ minWidth: '68px' }}></Column>
       <Column field="total" header="Total" body={totalBodyTemplate} bodyClassName={"text-end"} style={{ minWidth: '110px' }}></Column>
       <Column field='profit' header="Operational Profit" body={profitBodyTemplate} bodyClassName={"text-end"} style={{ minWidth: '144px' }} sortable></Column>
       <Column header="Duplicate" body={duplicateBodyTemplate} bodyClassName={"text-center"} style={{ minWidth: '82px' }}></Column>
