a0eee6689dd4980732bb76ec3a9149f038e0523e
Sourav Santra <55660986+Sourav59580@users.noreply.github.com>
2024-11-21 09:55:49 +0000

    update code

--- src/components/layout/settings/integrations/xero-integration.js
+++ src/components/layout/settings/integrations/xero-integration.js
@@ -3,16 +3,20 @@ import style from "./integration.module.scss";
 import { toast } from "sonner";
 
 const CLIENT_ID = "743BAB12B3A24534889C5C803A35E12C";
-const REDIRECT_URI = "https://dev.memate.com.au/settings/integrations";
 // const REDIRECT_URI = "http://localhost:3000/settings/integrations";
 
-const authorizationUrl = `https://login.xero.com/identity/connect/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=openid profile email offline_access accounting.transactions accounting.contacts accounting.settings&state=111`;
-
 const XeroIntegration = () => {
+  const REDIRECT_URI = window.location.href;
+  const authorizationUrl = `https://login.xero.com/identity/connect/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=openid profile email offline_access accounting.transactions accounting.contacts accounting.settings&state=111`;
+
   const [redirectParams, setRedirectParams] = useState(null);
 
+  const processedCodes = new Set();
   const exchangeCodeForToken = async (code) => {
+    if (processedCodes.has(code)) return;
+
     try {
+      processedCodes.add(code);
       const accessToken = localStorage.getItem("access_token");
 
       const response = await fetch(`${process.env.REACT_APP_BACKEND_API_URL}/xero?code=${code}`, {
@@ -26,7 +30,9 @@ const XeroIntegration = () => {
     } catch (error) {
       console.error("Error saving template:", error);
       toast.error("Failed to send the xero code. Please try again.");
-    }
+    }  finally {
+        processedCodes.delete(code);
+     }
   };
 
   const handleButtonClick = () => {
@@ -65,7 +71,7 @@ const XeroIntegration = () => {
         clearInterval(timer);
         console.log("Popup closed before completing login.");
       }
-    }, 500);
+    }, 1000);
   };
 
   return (
