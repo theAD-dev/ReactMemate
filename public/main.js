0d22e2f83f2b6c1f6d7c1a49c35a324d7928d1f9
soura1121 <sourav.1121@zenmonk.tech>
2024-12-11 17:20:39 +0530

    feature: desktop application

---
+++ public/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow, screen } = require('electron/main')
+
+function createWindow () {
+  const primaryDisplay = screen.getPrimaryDisplay();
+  const { width, height } = primaryDisplay.workAreaSize;
+
+  const win = new BrowserWindow({
+    width: width,
+    height: height,
+    webPreferences: {
+      enableRemoteModule: true
+    }
+  })
+
+  win.loadURL('http://localhost:3000')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
