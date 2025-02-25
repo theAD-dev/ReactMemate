ad181351199bde1efbb8dd1e72e3984ceb8ed0a1
soura1121 <sourav.1121@zenmonk.tech>
2025-02-25 16:22:26 +0530

    feature: resend quote api update

--- src/components/Business/features/sales-features/resend-quote/resend-quote.jsx
+++ src/components/Business/features/sales-features/resend-quote/resend-quote.jsx
@@ -2,7 +2,7 @@ import { toast } from "sonner";
 import { useState } from "react";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { getClientById } from "../../../../../APIs/ClientsApi";
-import { sendComposeEmail } from "../../../../../APIs/management-api";
+import { resendQuoteEmail } from "../../../../../APIs/management-api";
 import SendDynamicEmailForm from "../../../../../ui/send-email-2/send-email";
 
 const ResendQuoteEmail = ({ viewShow, setViewShow, clientId, projectId, projectCardData }) => {
@@ -16,7 +16,7 @@ const ResendQuoteEmail = ({ viewShow, setViewShow, clientId, projectId, projectC
     });
 
     const mutation = useMutation({
-        mutationFn: (data) => sendComposeEmail(projectId, "", data),
+        mutationFn: (data) => resendQuoteEmail(projectId, data),
         onSuccess: (response) => {
             setViewShow(false);
             projectCardData();
