337315c610636b8a759f4dfde822eeeed7ab03d2
Sourav Santra <55660986+Sourav59580@users.noreply.github.com>
2025-03-10 12:57:38 +0000

    craco configure

--- craco.config.js
+++ craco.config.js
@@ -4,6 +4,12 @@ module.exports = {
     webpack: {
         alias: {
             "@": path.resolve(__dirname, "src"),
+            '@app': path.resolve(__dirname, 'src/app'),
+            '@features': path.resolve(__dirname, 'src/features'),
+            '@pages': path.resolve(__dirname, 'src/pages'),
+            '@entities': path.resolve(__dirname, 'src/entities'),
+            '@shared': path.resolve(__dirname, 'src/shared'),
+            '@components': path.resolve(__dirname, 'src/components')
         },
     },
 };
