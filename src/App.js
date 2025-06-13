8078e9aa1e322d6359ac97cf28f85c1d11b45f98
Sourav59580 <souravsantra59580@gmail.com>
2025-06-13 17:10:20 +0530

    refactor: remove commented-out code from App and index files for cleaner codebase

--- src/App.js
+++ src/App.js
@@ -67,3 +67,40 @@ function App() {
 }
 
 export default App;
+// import React from 'react';
+// import { HelmetProvider } from 'react-helmet-async';
+// import { BrowserRouter } from "react-router-dom";
+// import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+// import { PrimeReactProvider } from "primereact/api";
+// import ReactDOM from 'react-dom/client';
+// import { Toaster } from 'sonner';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import "primereact/resources/themes/lara-light-cyan/theme.css";
+// import './shared/styles/prime.css';
+// import './shared/styles/index.css';
+// import './shared/styles/App.css';
+// import './shared/styles/global.scss';
+// import App from './App';
+// import "@fontsource/inter";
+// import "@fontsource/roboto";
+// import { TrialHeightProvider } from './app/providers/trial-height-provider';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// const queryClient = new QueryClient();
+
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <HelmetProvider>
+//         <PrimeReactProvider>
+//           <TrialHeightProvider>
+//             <QueryClientProvider client={queryClient}>
+//               <Toaster expand={true} richColors closeButton position="top-right" />
+//               <App />
+//             </QueryClientProvider>
+//           </TrialHeightProvider>
+//         </PrimeReactProvider>
+//       </HelmetProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
