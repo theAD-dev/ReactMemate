ce89e4358557bb0f40104df07dc8a57943c179bf
Sourav59580 <souravsantra59580@gmail.com>
2025-09-15 10:49:45 +0530

    fix: Update jobProjectId prop in CreateJob component to handle visibility state

--- src/components/Business/Pages/management/project-card/project-card-model.js
+++ src/components/Business/Pages/management/project-card/project-card-model.js
@@ -890,7 +890,7 @@ const ProjectCardModel = ({ viewShow, setViewShow, projectId, project, statusOpt
           </div>
         </Modal.Body>
       </Modal>
-      <CreateJob visible={visible} setVisible={setVisible} setRefetch={() => projectCardData(projectId)} jobProjectId={project?.value} />
+      <CreateJob visible={visible} setVisible={setVisible} setRefetch={() => projectCardData(projectId)} jobProjectId={visible ? project?.value : ""} />
       <NewExpensesCreate visible={createExpenseVisible} setVisible={setCreateExpenseVisible} setRefetch={() => projectCardData(projectId)} expenseProjectId={project?.value} />
     </>
   );
