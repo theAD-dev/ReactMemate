d07b440764ba9ae6b57e2e2d171714633a1bb764
Sourav59580 <souravsantra59580@gmail.com>
2025-10-09 10:23:30 +0530

    feat: Add assets navigation link and icon to header; enhance menu styling for active state

--- src/shared/ui/header/header.jsx
+++ src/shared/ui/header/header.jsx
@@ -8,6 +8,7 @@ import ProfileInfo from "./components/profile-info";
 import style from './header.module.scss';
 import { useAuth } from "../../../app/providers/auth-provider";
 import { useTrialHeight } from "../../../app/providers/trial-height-provider";
+import assetsIcon from '../../../assets/images/icon/assets.svg';
 import Briefcase from "../../../assets/images/icon/briefcase.svg";
 import calendarTick from "../../../assets/images/icon/calendar-tick.svg";
 import clipboardTick from "../../../assets/images/icon/clipboard-tick.svg";
@@ -208,6 +209,17 @@ const Header = () => {
                                                 >
                                                     <img src={SuppliersIcon} alt="SuppliersIcon" />
                                                     Suppliers
+                                                </NavLink>
+                                            </li>
+                                            <li>
+                                                <NavLink
+                                                    to="/assets"
+                                                    className={({ isActive }) =>
+                                                        (isActive ? "menuActive" : "link") + " assets"
+                                                    }
+                                                >
+                                                    <img src={assetsIcon} alt="assets" />
+                                                    Assets
                                                 </NavLink>
                                             </li>
                                         </ul>
