cdabc4e56645366157652d4aaf7c0d0542b4cdbd
Sourav59580 <souravsantra59580@gmail.com>
2026-01-20 21:17:27 +0530

    feat: Add organizationId parameter to getListOfForms API call and update EnquiryFormsTable to use session organization ID

--- src/APIs/enquiries-api.js
+++ src/APIs/enquiries-api.js
@@ -1,7 +1,7 @@
 import { fetchAPI } from "./base-api";
 const API_BASE_URL = process.env.REACT_APP_BACKEND_API_URL;
 
-export const getListOfForms = async (page, limit, search = "", order = "", isShowDeleted) => {
+export const getListOfForms = async (page, limit, search = "", order = "", isShowDeleted, organizationId) => {
   const offset = (page - 1) * limit;
   const endpoint = `/inquiries/forms/`;
   const options = {
@@ -10,7 +10,7 @@ export const getListOfForms = async (page, limit, search = "", order = "", isSho
   const url = new URL(`${API_BASE_URL}${endpoint}`);
   url.searchParams.append("limit", limit);
   url.searchParams.append("offset", offset);
-  url.searchParams.append("organization", 5);
+  if (organizationId) url.searchParams.append("organization", organizationId);
   if (search) url.searchParams.append("search", search);
   if (order) url.searchParams.append("ordering", order);
   if (isShowDeleted) url.searchParams.append('deleted', 1);
